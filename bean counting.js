bc3dfdb793d3dc8e0e254e8cc877aa854ef3c615
Mepposan <34914864+Mepposan@users.noreply.github.com>
2017-12-28 14:14:08 +0100

    bean counting solution
    bean counting exercise solution
    http://eloquentjavascript.net/03_functions.html

---
+++ bean counting.js
@@ -0,0 +1,32 @@
+function countBs(string){
+var position = 0
+var result = 0
+while (position < string.length) {
+  if (string.charAt(position) == "B") {
+    result = result + 1; 
+    position = position + 1;
+  }
+  else
+    position = position + 1;
+}
+return result
+}
+
+function countChar(string, B){
+var position = 0
+var result = 0
+while (position < string.length) {
+  if (string.charAt(position) == B) {
+    result = result + 1; 
+    position = position + 1;
+  }
+  else
+    position = position + 1;
+}
+return result
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4

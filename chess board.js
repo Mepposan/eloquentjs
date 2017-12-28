a2a7c07f80d23470e74b5b9a6ca03b6aac0de7e7
Mepposan <34914864+Mepposan@users.noreply.github.com>
2017-12-28 14:09:43 +0100

    chess board solution
    chess board exercise solution
    http://eloquentjavascript.net/02_program_structure.html

---
+++ chess board.js
@@ -0,0 +1,30 @@
+var rzad1 = " ";
+var rzad2 = "#";
+var wiersze = 0;
+var size = 8;
+
+while (wiersze < size);
+{
+  if (rzad1.length < size && rzad1.length % 2 != 0);
+  rzad1 = rzad1 + "#";
+
+  else if (rzad1.length < size && rzad1.length % 2 == 0);
+  rzad1 = rzad1 + " ";
+
+  else if (rzad2.length < size && rzad2.length % 2 != 0);
+  rzad2 = rzad2 + " ";
+
+  else if (rzad2.length < size && rzad2.length % 2 == 0);
+  rzad2 = rzad2 + "#";
+
+  else if ((rzad1.length >= size) && (rzad2.length >= size) && (wiersze % 2 == 0));
+  {
+  console.log(rzad1);
+  wiersze = wiersze + 1;
+  }
+
+  else if ((rzad1.length >= size) && (rzad2.length >= size) && (wiersze % 2 != 0));
+  {
+  console.log(rzad2);
+  wiersze = wiersze + 1;
+  }}

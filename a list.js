5d6fd2fecd6c5a620fa41eed2d5b9a5ec0e3c076
Mepposan <meppo.san@gmail.com>
2018-01-04 18:47:09 +0100

    function nth works, 4/4

--- a list.js
+++ a list.js
@@ -37,8 +37,22 @@ var list = {value: value,
 return list
 }
 
-
   console.log(prepend(10, prepend(20, null)));
   // → {value: 10, rest: {value: 20, rest: null}}
-  console.log(nth(arrayToList([10, 20, 30]), 1));
+
+function nth(list, index) {
+var arr = 0
+var x = 1
+  for (i = 0; i < x; i++) {
+    if (x == index + 1)  	
+    	arr = list.value;
+    else {
+      list = list.rest;
+      x++;
+    }
+  }
+return arr;
+}
+
+console.log(nth(arrayToList([10, 20, 30]), 1));
   // → 20

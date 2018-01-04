544afaa0e10824080b3c43609ba2a0ac610be08a
Mepposan <meppo.san@gmail.com>
2018-01-04 19:27:06 +0100

    kosmetyka

--- a list.js
+++ a list.js
@@ -1,31 +1,27 @@
 // Your code here.
 function arrayToList(array) {
   var list = null;
-  for (i = array.length -1; i > -1; i--) {
-    list = {
-      value: array[i],
-      rest: list,
-    }
-    array == array.slice(i)
-    }
-    return list
-  }
-  console.log(arrayToList([10, 20, 30, 40, 50, 60]));
+  for (i = array.length -1; i > -1; i--) 
+    list = {value: array[i], rest: list};
+  return list;
+}
+
+console.log(arrayToList([10, 20]));
   // → {value: 10, rest: {value: 20, rest: null}}
 
-  function listToArray(list) {
-  var arr = [];
+function listToArray(list) {
+  var array = [];
   var x = 1
   for (i = 0; i < x; i++) {
       if (list.rest != null) {
-      	arr.push(list.value);
+      	array.push(list.value);
       	list = list.rest;
       	x++
       	}
       else
-      	arr.push(list.value);
+      	array.push(list.value);
      }
-return arr;
+return array;
 }
   
   console.log(listToArray(arrayToList([10, 20, 30, 40, 50, 60])));
@@ -41,17 +37,17 @@ return list
   // → {value: 10, rest: {value: 20, rest: null}}
 
 function nth(list, index) {
-var arr = 0
+var n = 0
 var x = 1
   for (i = 0; i < x; i++) {
     if (x == index + 1)  	
-    	arr = list.value;
+    	n = list.value;
     else {
       list = list.rest;
       x++;
     }
   }
-return arr;
+return n;
 }
 
 console.log(nth(arrayToList([10, 20, 30]), 1));

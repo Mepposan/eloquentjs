e436a72e87d0a4aa65883834acdb7be42acf2bf7
Mepposan <meppo.san@gmail.com>
2018-01-04 17:10:42 +0100

    function prepend works

--- a list.js
+++ a list.js
@@ -12,26 +12,32 @@ function arrayToList(array) {
   }
   console.log(arrayToList([10, 20, 30, 40, 50, 60]));
   // → {value: 10, rest: {value: 20, rest: null}}
-  
-
 
   function listToArray(list) {
   var arr = [];
   var x = 1
   for (i = 0; i < x; i++) {
       if (list.rest != null) {
-      arr.push(list.value);
-      list = list.rest;
-      x++
-      }
-    else
-      arr.push(list.value)
-      }
-      return arr
+      	arr.push(list.value);
+      	list = list.rest;
+      	x++
+      	}
+      else
+      	arr.push(list.value);
+     }
+return arr;
 }
   
   console.log(listToArray(arrayToList([10, 20, 30, 40, 50, 60])));
   // → [10, 20, 30]
+
+function prepend(value, rest) {
+var list = {value: value,
+	    	rest: rest,}
+return list
+}
+
+
   console.log(prepend(10, prepend(20, null)));
   // → {value: 10, rest: {value: 20, rest: null}}
   console.log(nth(arrayToList([10, 20, 30]), 1));
